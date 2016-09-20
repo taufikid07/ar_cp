4aa39f29f362d982adfb82a23ff08c90d38fe79e
akbar03 <bantatgunners@gmail.com>
2016-09-20 21:18:50 +0700

    Update AR KTP

---
+++ Fazryan/AR_KartuTandaPenduduk/Assets/LoadingScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadingScript : MonoBehaviour
+{
+
+    public float loadingTime;
+    public Image loadingBar;
+
+    void Start()
+    {
+
+        loadingBar.fillAmount = 0;
+
+    }
+
+
+    void Update()  {
+
+        if (loadingBar.fillAmount <= 1)
+        {
+            loadingBar.fillAmount += 1.0f / loadingTime * Time.deltaTime;
+
+        }
+         if (loadingBar.fillAmount == 1.0f) {
+            Application.LoadLevel("KTP");
+
+
+        }
+
+
+    }
+}
